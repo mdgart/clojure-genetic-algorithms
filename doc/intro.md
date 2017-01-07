3ba7ab7f30bfe72361ccd995406aa7264f12af5d
Mauro De Giorgi <mauro@momentfeed.com>
2017-01-07 10:10:14 -0700

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to genetic-algorithms
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/genetic_algorithms/core_test.clj
@@ -0,0 +1,7 @@
+(ns genetic-algorithms.core-test
+  (:require [clojure.test :refer :all]
+            [genetic-algorithms.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
